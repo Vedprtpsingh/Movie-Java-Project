427226fd7aec126fc1e4a336c22086129a896864
vedprtpsingh <singhvedprartap14@gmail.com>
2024-10-06 11:50:09 +0530

    first commit

---
+++ src/main/java/dev/ved/movies/Review.java
@@ -0,0 +1,16 @@
+package dev.ved.movies;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+@DataAmount(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private Object id;
+    private String body;
+}
